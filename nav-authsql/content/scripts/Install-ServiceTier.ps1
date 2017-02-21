63cd143053aefe09619640262607c62e5e9b29a3
tfenster <tfenster@infoma.de>
2017-02-21 00:19:52 +0000

    copy missing dlls here

--- nav-authsql/content/scripts/Install-ServiceTier.ps1
+++ nav-authsql/content/scripts/Install-ServiceTier.ps1
@@ -3,6 +3,10 @@ param(
     [String]$SERVERINSTANCE
 )
 
+$filesToCopy = @("Microsoft.IdentityModel.dll", "Microsoft.ReportViewer.Common.dll", "Microsoft.ReportViewer.DataVisualization.dll", "Microsoft.ReportViewer.ProcessingObjectModel.dll", `
+    "Microsoft.ReportViewer.WinForms.dll", "Microsoft.SqlServer.Types.dll")
+$currDir = Split-Path $MyInvocation.MyCommand.Definition
+
 $process = 'msiexec.exe'
 $params = " SERVERINSTANCE=" + $SERVERINSTANCE
 $arguments = '/i "C:\\install\\content\\DynamicsNavDvd\\ServiceTier\\Microsoft Dynamics NAV Service.msi" /quiet /qn /norestart /log "C:\\install\\content\\LOG\\installnst.log"' + $params
@@ -18,5 +22,8 @@ while ($res.HasExited -eq $false) {
 $navSvc = Get-Service '*MicrosoftDynamicsNavServer*' 
 Set-Service $navSvc.Name -StartupType Disabled
 Stop-Service $navSvc.Name
+
+# Copy missing dlls
+& (Join-Path $currDir Copy-ItemsTo.ps1) -ParentDirectory 'c:\install\content' -FileNames $filesToCopy -TargetDirectory "c:\windows\system32"
 
 $exitCode = $res.ExitCode
