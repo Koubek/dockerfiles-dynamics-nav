7d80425be84b23c13fce845e041ebb0d3a53c7bf
Koubek <vanak.jakub@gmail.com>
2017-02-21 17:07:04 +0100

    Tag management + cleanup.
    Improved docker image tag management - there is a new script that will
    find NAV ProductVersion and this will automatically appear as a docker
    image tag.
    
    Cleanup of the the _*.bat files - there is only one right now that will
    preconfigure all required variables and this _presetvars.bat is being
    called from the rest of the files.

--- nav-authsql/_cleanup.bat
+++ nav-authsql/_cleanup.bat
@@ -1,6 +1,4 @@
-set /p host=<__privatereponame.txt
-set /p ImageName=<__imagename.txt
-set ContainerName=%ImageName::=_%
+call _presetvars.bat
 
 docker rm -f %ContainerName%
 docker rmi -f %host%/%ImageName%
--- nav-authsql/_run.detached.bat
+++ nav-authsql/_run.detached.bat
@@ -1,6 +1,4 @@
-set /p host=<..\__privatereponame.txt
-set /p ImageName=<__imagename.txt
-set ContainerName=%ImageName::=_%
+call _presetvars.bat
 
 docker rm %ContainerName%
 docker run -d --hostname=NAVSERVER -p 7045-7048:7045-7048 -e "sql_server=sql_ip\sql_instance" -e "sql_db=navdbname" -e "sql_user=user" -e "sql_pwd=pwd" -e "nav_user=navuser" -e "nav_user_pwd=pwd" -e "import_cronus_license=false" --name %ContainerName% %host%/%ImageName%
