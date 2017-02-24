72da2f43f0aed4702aefbbe9d9911255894c559c
Koubek <vanak.jakub@gmail.com>
2017-02-24 23:46:09 +0100

    Multi-layer docker compose
    Split the docker-compose into multiple docker compose files to achieve
    more flexible and configurable environment.

---
+++ nav-sql-compose/compose.up.nav-image.bat
@@ -0,0 +1,3 @@
+docker-compose -f docker-compose.yml -f docker-compose.nav-image.yml -f docker-compose.configs.yml up -d
+powershell docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' navsqlcompose_nav_1
+powershell docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' navsqlcompose_sql_1
