6523df80c9ed44236d5480e5ae2645ec6111f86f
Koubek <vanak.jakub@gmail.com>
2017-03-24 00:18:14 +0100

    Added powershell modules for NAV
    Added powershell modules for NAV management.

---
+++ root/__content/NAV/scripts/Docker.NAV.ModuleManagement/Docker.NAV.ModuleManagement.psm1
@@ -0,0 +1,6 @@
+﻿Get-Item $PSScriptRoot | Get-ChildItem -Recurse -Filter '*.ps1' |  Sort Name | foreach {
+    Write-Verbose "Loading $($_.Name)"  
+    . $_.fullname
+}
+
+Export-ModuleMember -Function '*'
